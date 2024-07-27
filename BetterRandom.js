61051ecddc4242ed1912539e8f920508b54984d6
Joel E. Mason <42191306+JEMasonMedia@users.noreply.github.com>
2024-07-27 09:37:22 -0500

    Update BetterRandom.js

--- BetterRandom.js
+++ BetterRandom.js
@@ -11,3 +11,25 @@ export default function BetterRandom(min, max, depth = 1000) {
 
   return accumulator[randomNumberInRange(0, accumulator.length - 1)]
 }
+
+
+/*
+function cryptoRandomNumberInRange(min, max) {
+  const range = max - min + 1;
+  const randomBytes = new Uint32Array(1);
+  crypto.getRandomValues(randomBytes);
+  const randomNumber = randomBytes[0] / (0xFFFFFFFF + 1);
+  return Math.floor(randomNumber * range) + min;
+}
+
+export default function BetterRandom(min, max, depth = 1000) {
+  let accumulator = [];
+
+  for (let i = 0; i < depth; i++) {
+    accumulator.push(cryptoRandomNumberInRange(min, max));
+  }
+
+  const randomIndex = cryptoRandomNumberInRange(0, accumulator.length - 1);
+  return accumulator[randomIndex];
+}
+*/
